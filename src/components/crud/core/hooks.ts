50c52314882569967107d5357eece9c809bab256
MingYang <li1342554547@gmail.com>
2025-12-10 13:41:49 +0800

    refactor: 将 `useCrudCore` 函数从 `index.ts` 移动到 `hooks.ts` 文件中

---
+++ src/components/crud/core/hooks.ts
@@ -0,0 +1,32 @@
+import Mitt from "../../../utils/mitt"
+import { merge } from "@fonds/utils"
+import { useConfig } from "../../../hooks"
+import { createHelper } from "./helper"
+import { createService } from "./service"
+import { createCrudContext } from "./context"
+
+export function useCrudCore(options: { id?: string | number }) {
+  const mitt = new Mitt()
+  const { dict, permission } = useConfig()
+
+  const { crud, config, useCrudOptions } = createCrudContext({
+    id: options.id,
+    dict,
+    permission,
+    mitt,
+  })
+
+  const helper = createHelper({ config, crud, mitt })
+  const service = createService({
+    config,
+    crud,
+    mitt,
+    paramsReplace: helper.paramsReplace,
+  })
+
+  merge(crud, helper, service)
+
+  crud.use = useCrudOptions
+
+  return { crud, mitt }
+}
--- src/components/crud/core/index.ts
+++ src/components/crud/core/index.ts
@@ -1,36 +1,4 @@
-import Mitt from "../../../utils/mitt"
-import { merge } from "@fonds/utils"
-import { useConfig } from "../../../hooks"
-import { createHelper } from "./helper"
-import { createService } from "./service"
-import { createCrudContext } from "./context"
-
-export function useCrudCore(options: { id?: string | number }) {
-  const mitt = new Mitt()
-  const { dict, permission } = useConfig()
-
-  const { crud, config, useCrudOptions } = createCrudContext({
-    id: options.id,
-    dict,
-    permission,
-    mitt,
-  })
-
-  const helper = createHelper({ config, crud, mitt })
-  const service = createService({
-    config,
-    crud,
-    mitt,
-    paramsReplace: helper.paramsReplace,
-  })
-
-  merge(crud, helper, service)
-
-  crud.use = useCrudOptions
-
-  return { crud, mitt }
-}
-
 export * from "./context"
 export * from "./helper"
+export * from "./hooks"
 export * from "./service"
