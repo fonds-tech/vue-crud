7daeb08116270d19b20a7ec5ed0e195c5d4b2447
MingYang <li1342554547@gmail.com>
2025-12-14 20:08:37 +0800

    feat: 导出所有主要组件和相关类型。

--- src/entry.ts
+++ src/entry.ts
@@ -20,3 +20,30 @@ const Crud = {
 }
 
 export { Crud }
+
+// ===================== 组件导出 =====================
+export { default as FdAddButton } from "./components/add-button"
+export { default as FdCascader } from "./components/cascader"
+export { ContextMenu, contextMenu, default as FdContextMenu } from "./components/context-menu"
+export { default as FdCrud } from "./components/crud"
+// ===================== 类型导出 =====================
+export * as CrudTypes from "./components/crud/interface"
+export { default as FdDeleteButton } from "./components/delete-button"
+export { default as FdDetail } from "./components/detail"
+export * as DetailTypes from "./components/detail/interface"
+export { default as FdDialog } from "./components/dialog"
+export { default as FdExport } from "./components/export"
+export { default as FdForm } from "./components/form"
+export * as FormTypes from "./components/form/interface"
+export { default as FdGrid } from "./components/grid"
+export { FdGridItem } from "./components/grid-item"
+export { default as FdImport } from "./components/import"
+export { default as FdSearch } from "./components/search"
+
+export * as SearchTypes from "./components/search/interface"
+export { default as FdSelect } from "./components/select"
+export { default as FdTable } from "./components/table"
+export * as TableTypes from "./components/table/interface"
+export { default as FdUpsert } from "./components/upsert"
+export * as UpsertTypes from "./components/upsert/interface"
+export * from "./types/config"
